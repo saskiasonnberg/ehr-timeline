3afe456190dae7913f2867de6c02c7fb748eb4c5
sonnbsas <saskia.sonnberg@tieto.com>
2020-04-20 21:23:35 +0300

    initial timeline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HorizontalTimeline from "react-horizontal-timeline";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const VALUES = ["1991-05-01", "1991-06-01", "1991-07-01","1991-08-01","1991-09-01"];
+export const DESCRIPTIONS = ["Taotlus esitatud", "Menetlus algatatud", "1. Kooskõlastusring alanud", "Edastatud märkuste lahendmiseks", "Märkused lahendatud"]
+
+export default class App extends React.Component {
+  state = { value: 0, previous: 0 }; 
+   
+  render() {   
+    return (
+      <div>
+        {/* Bounding box for the Timeline */}
+        <div style={{ width: "60%", height: "100px", margin: "30px auto" }}>
+          <HorizontalTimeline
+           // index={this.state.value}            
+            index={VALUES.length-1}
+            indexClick={(index) => {
+              this.setState({ value: index, previous: this.state.value });
+            }}
+            values={VALUES}
+            minEventPadding={80}
+            maxEventPadding={80}
+            styles = {{ background: '#AAE1F8', foreground: '#0083BA', outline: '#dfdfdf' }}
+            getLabel ={(VALUES, index) => ((new Date(VALUES)).toDateString().substring(4)+" "+DESCRIPTIONS[index])}            
+            labelWidth={170}
+          />          
+        </div>
+        <div className="text-center">
+          {/* any arbitrary component can go here */}
+          {/*{this.state.value}*/}          
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
